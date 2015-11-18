c425c6f9634053db32b6cd7a3040b030248a148c
Kyunghyun <khpaeng@kaist.ac.kr>
2015-11-18 23:58:04 +0900

    Add a net speed check script

--- 0__ATTENTION/net_speed_check.sh
+++ 0__ATTENTION/net_speed_check.sh
@@ -1,9 +1,10 @@
 
 CAFFE_PATH=/home/paeng/projects/1__LIB/caffe-paeng
 
-$CAFFE_PATH/build/tools/caffe time \
+$CAFFE_PATH/build/tools/caffe ftime \
   -gpu $1 \
-  -model 0__MODELS/$2/$3/train.prototxt \
+  -model 0__MODELS/$2/$3/train_for_speed.prototxt \
+  -weights 0__MODELS/$2/$3/ft_models/attention_imagenet_loc_15_iter_153824.caffemodel \
   -iterations 20
 
 
